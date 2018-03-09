9c3d7015524325e0fd6af8f5a78da3f9d1fefd91
Simon Atkins <this-is-simon@github.com>
2018-03-09 15:48:05 +0000

    still struggling

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,11 +2,13 @@ def pet_shop_name(shop)
   shop[:name]
 end
 
-def total_cash()
-end
+# def total_cash(cash_amount)
+#   return admin[total_cash:]
+# end
 
-def add_two_things(x, y)
-  return x + y
-end
 
-add_two_things(3, 4)
+# def add_two_things(x, y)
+#   return x + y
+# end
+#
+# add_two_things(3, 4)
